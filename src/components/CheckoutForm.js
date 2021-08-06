3a8fbc66e8825b97f5e4436ef9989e08b76e6382
Joe Escobedo <joe_escob1023@live.com>
2021-08-06 14:38:13 -0700

    unsure if my tests work, Im getting errors if I run npm test. I belive the tests would correctly pass

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -32,7 +32,7 @@ const CheckoutForm = (props) => {
     <>
       <form onSubmit={handleSubmit}>
         <h2>Checkout Form</h2>
-        <label>
+        <label htmlFor="firstName">
           First Name:
           <input
             name="firstName"
@@ -40,7 +40,7 @@ const CheckoutForm = (props) => {
             onChange={handleChanges}
           />
         </label>
-        <label>
+        <label htmlFor="lastName">
           Last Name:
           <input
             name="lastName"
@@ -48,7 +48,7 @@ const CheckoutForm = (props) => {
             onChange={handleChanges}
           />
         </label>
-        <label>
+        <label htmlFor="address">
           Address:
           <input
             name="address"
@@ -56,15 +56,15 @@ const CheckoutForm = (props) => {
             onChange={handleChanges}
           />
         </label>
-        <label>
+        <label htmlFor="city">
           City:
           <input name="city" value={values.city} onChange={handleChanges} />
         </label>
-        <label>
+        <label htmlFor="state">
           State:
           <input name="state" value={values.state} onChange={handleChanges} />
         </label>
-        <label>
+        <label htmlFor="zip">
           Zip:
           <input name="zip" value={values.zip} onChange={handleChanges} />
         </label>
--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,44 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  render(<CheckoutForm />);
 
-test("form shows success message on submit with form details", () => {});
+  const header = screen.queryByText("Checkout Form");
+
+  expect(header).toBeInTheDocument();
+});
+
+test("form shows success message on submit with form details", () => {
+  render(<CheckoutForm />);
+
+  const firstName = screen.getByLabelText(/first name/i);
+  const lastName = screen.getByLabelText(/last name/i);
+  const address = screen.getByLabelText(/address/i);
+  const city = screen.getByLabelText(/city/i);
+  const state = screen.getByLabelText(/state/i);
+  const zip = screen.getByLabelText(/zip/i);
+
+  const button = screen.getByRole("button", { value: /Checkout/i });
+
+  userEvent.type(firstName, "Joe");
+  userEvent.type(lastName, "Escobedo");
+  userEvent.type(address, "123 Sun St");
+  userEvent.type(city, "Las Vegas");
+  userEvent.type(state, "NV");
+  userEvent.type(zip, "89149");
+  userEvent.click(button);
+
+  const newCustomer = screen.findByText(/Joe/i);
+  expect(firstName).toHaveValue("Joe");
+  expect(lastName).toHaveValue("Escobedo");
+  expect(address).toHaveValue("123 Sun St");
+  expect(city).toHaveValue("Las Vegas");
+  expect(state).toHaveValue("NV");
+  expect(zip).toHaveValue("89149");
+  expect(newCustomer).toBeTruthy();
+});
